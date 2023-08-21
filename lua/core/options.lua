47dce69ffe57639f662d0d3981814e8211719231
mihir-kurkure <mihir.k98@pm.me>
2023-08-21 12:17:56 -0400

    changes to indent options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,18 +1,23 @@
 local opts = {
-	shiftwidth = 4,
-	tabstop = 4,
-	expandtab = true,
-	wrap = false,
-	termguicolors = true,
-	number = true,
-	relativenumber = false,
+  shiftwidth = 4,
+  tabstop = 4,
+  expandtab = true,
+  wrap = false,
+  termguicolors = true,
+  number = true,
+  relativenumber = false,
 }
 
 -- Set options from table
 for opt, val in pairs(opts) do
-	vim.o[opt] = val
+  vim.o[opt] = val
 end
 
 -- Set other options
 local colorscheme = require("helpers.colorscheme")
 vim.cmd.colorscheme(colorscheme)
+
+--indent
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+--vim.opt.listchars:append "eol:↴"
--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,13 @@
-return { "lukas-reineke/indent-blankline.nvim" },
-
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup {
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+        show_char_blankline = " ",
+      }
+    end
+  },
 }
