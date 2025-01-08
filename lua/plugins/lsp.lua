8610d74744a76c143081c7306f692cd2a68ed964
Mihir K <mihir.k98@pm.me>
2025-01-08 09:27:41 -0500

    Lsp update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,11 +19,11 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
-					"pylsp",
+					-- "pylsp",
 					"rust_analyzer",
 					"clangd",
 				},
-				automatic_installation = true,
+				automatic_installation = false,
 			})
 
 			-- Quick access via keymap
@@ -115,7 +115,34 @@ return {
 			})
 
 			-- Python
-			require("lspconfig")["pylsp"].setup({})
+			require("lspconfig")["pyright"].setup({
+				on_attach = on_attach,
+				-- settings = {
+				-- 	pylsp = {
+				-- 	plugins = {
+				-- 		-- formatter options
+				-- 		black = { enabled = true },
+				-- 		autopep8 = { enabled = false },
+				-- 		yapf = { enabled = false },
+				-- 		-- linter options
+				-- 		pylint = { enabled = true, executable = "pylint" },
+				-- 		pyflakes = { enabled = false },
+				-- 		pycodestyle = { enabled = false },
+				-- 		-- type checker
+				-- 		pylsp_mypy = { enabled = true },
+				-- 		-- auto-completion options
+				-- 		jedi_completion = { fuzzy = true },
+				-- 		-- import sorting
+				-- 		pyls_isort = { enabled = true },
+				-- 	},
+				-- 	},
+				-- },
+				-- flags = {
+				-- 	debounce_text_changes = 200,
+				-- },
+				capabilities = capabilities,
+				}
+			)
 
 			--Rust
 			--require("lspconfig")["rust_analyzer"].setup({
